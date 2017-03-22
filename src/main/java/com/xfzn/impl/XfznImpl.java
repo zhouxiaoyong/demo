bf474fa6f3d8d3261d0526bfe9a2d338f81fb170
asdf123 <zhouxiaoyongsay@163.com>
2017-03-22 10:39:18 +0800

    API-IMPl

---
+++ src/main/java/com/xfzn/impl/XfznImpl.java
@@ -0,0 +1,24 @@
+package com.xfzn.impl;
+
+import java.util.List;
+
+import com.xfzn.dao.XfznDao;
+
+public class XfznImpl implements XfznDao<String> {
+
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void find() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<String> findList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
